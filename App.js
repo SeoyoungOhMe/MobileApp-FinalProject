d15ad3a06d273cbafc467de327a2e49d0e6a9f5f
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-28 14:32:49 +0900

    navigation

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+var title_st = {fontSize:50, margin:10};
+
+
+function StartScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={title_st}>꼰대 테스트</Text>
+      <Button title="테스트 시작하기"
+        onPress={ function(){ navigation.navigate('Test') } } />
+    </View>
+  );
+}
+
+function TestScreen() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+function ResultScreen() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
