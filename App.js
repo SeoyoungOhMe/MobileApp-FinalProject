5abd0fee3e45d9969c664dbb3a2351f7ef8168fd
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-28 21:22:59 +0900

    TestScreen1~3

--- App.js
+++ App.js
@@ -10,37 +10,103 @@ function StartScreen({ navigation }) {
       <Text style={styles.title_st}>꼰대 테스트</Text>
       <View style={{height:50}}></View>
       <Button title="테스트 시작하기"
-        onPress={ function(){ navigation.navigate('Test') } } />
+        onPress={ function(){ navigation.navigate('Test1') } } />
     </View>
   );
 }
 
-function Question(props) {
+function TestScreen1({ navigation }) {
 
-  const [ count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <View>
-      <Text style={styles.text_st}>{props.content}</Text>
+      <Text style={styles.text_st}>1. "나 정도면 꼰대 아니지 않아?"라고 생각한 적이 있다.</Text>
       <View style={{margin:10, flexDirection:"row"}}>
         <Button title="네"
-          onPress={ function(){ setCount(count+1); } } />
+          onPress={ function(){ setCount(count + 1); navigation.navigate('Test2', { count }); } } />
         <Button title="아니요"
-          onPress={ function(){  } } />  
+          onPress={ function(){ navigation.navigate('Test2', { count }); } } />  
       </View>
     </View>
-  )
+  );
 }
 
-function TestScreen() {
+function TestScreen2({ navigation, route }) {
+
+  const { count } = route.params;
+  const [count2, setCount2] = useState(count);
+
   return (
     <View>
-      <Question content="1. '나 정도면 꼰대 아니지 않나?'라는 생각을 한 적이 있다." />
-      <Question content="2. " />
-      <Question content="3. " />
-      <Question content="4. " />
-      <Question content="5. " />
-      <Question content="6. " />
+      <Text>{count}</Text>
+      <Text>{count2}</Text>
+
+      <Text style={styles.text_st}>2. 처음 만났지만 나보다 나이가 어리면 반말을 한다.</Text>
+      <View style={{margin:10, flexDirection:"row"}}>
+        <Button title="네"
+          onPress={ function(){ 
+            setCount2(count2 + 1); 
+            alert(count2);
+            navigation.navigate('Test3', { count2 }); 
+            } } />
+        <Button title="아니요"
+          onPress={ function(){ 
+            alert(count2);
+            navigation.navigate('Test3', { count2 } ); 
+          } } />  
+      </View>
+    </View>
+  );
+}
+
+function TestScreen3({ navigation, route }) {
+
+  const { count2 } = route.params;
+  const [count3, setCount3] = useState(count2);
+
+  return (
+    <View>
+      <Text>{count2}</Text>
+      <Text>{count3}</Text>
+
+      <Text style={styles.text_st}>3. 내 의견과 반대되는 의견을 내는 사람이 못마땅하다.</Text>
+      <View style={{margin:10, flexDirection:"row"}}>
+        <Button title="네"
+          onPress={ function(){ 
+            alert(count);alert(count3);
+            setCount3(count3 + 1); 
+            navigation.navigate('Test4', { count3 }); 
+          } } />
+        <Button title="아니요"
+          onPress={ function(){ 
+            navigation.navigate('Test4', { count3 } ); 
+            } } />  
+      </View>
+    </View>
+  );
+}
+
+function TestScreen4({ navigation, route }) {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+function TestScreen5({ navigation, route }) {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+function TestScreen6({ navigation, route }) {
+  return (
+    <View>
+      
     </View>
   );
 }
@@ -63,7 +129,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Start" component={StartScreen} />
-        <Stack.Screen name="Test" component={TestScreen} />
+        <Stack.Screen name="Test1" component={TestScreen1} />
+        <Stack.Screen name="Test2" component={TestScreen2} />
+        <Stack.Screen name="Test3" component={TestScreen3} />
+        <Stack.Screen name="Test4" component={TestScreen4} />
+        <Stack.Screen name="Test5" component={TestScreen5} />
+        <Stack.Screen name="Test6" component={TestScreen6} />
         <Stack.Screen name="Result" component={ResultScreen} />
       </Stack.Navigator>
     </NavigationContainer>
